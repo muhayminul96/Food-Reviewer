d64a85242acf3a5d9b307b8d977e6e779f2d619e
muhayminul <muhayminul96@gmail.com>
2022-04-04 23:20:47 +0600

    navbar added

---
+++ src/components/Head/Head.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Head.css'
+
+const Head = () => {
+    return (
+        <div className='nav-bar'>
+            <div className='nav-menu'><a href='#'>Home</a></div>
+            <div className='nav-menu'><a href='#'>Review</a></div>
+            <div className='nav-menu'><a href='#'>Blog</a></div>
+            <div className='nav-menu'><a href='#'>About</a></div>
+            <div className='nav-menu'><a href='#'>DashBoard</a></div>
+
+           
+        </div>
+    );
+};
+
+export default Head;
