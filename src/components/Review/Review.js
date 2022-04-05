82c42cae21f6e8731bc3ead1c5b1e5dd97f82166
muhayminul <muhayminul96@gmail.com>
2022-04-05 12:57:43 +0600

    add star and picture of reviewer

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import './Review.css'
+import ReactStars from "react-rating-stars-component";
+
+
 const Review = (props) => {
-    const {name,rating,reviewText} = props.review
+    const {picture,name,rating,reviewText} = props.review
+
     return (
         <div className='review-info'>
-            <p> rating {rating}</p>
+             <div className="stars">
+             <ReactStars
+                count={rating} 
+                size={34}
+                color="#333C83"
+                activeColor="#333C83"
+             />
+             <ReactStars
+                count={5-rating}
+                size={34}
+                color='#F24A72'
+                activeColor='#F24A72'
+                
+             />
+  
+             </div>
+             
             <p title={reviewText}>{reviewText.length > 150 ? reviewText.slice(0,147) + '...' : reviewText}</p>
             
             <div className='reviewer-info'>
+                <img src={picture} alt="" />
                 <h3>{name}</h3>
+                
             </div>
             
         </div>
