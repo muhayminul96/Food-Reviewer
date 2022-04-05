f4e16f32821dd2d33f7d6d6e0bdfaa2cf24c035a
muhayminul <muhayminul96@gmail.com>
2022-04-05 16:11:10 +0600

    not found page updated

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>not found 404</h1>
+        <div className='not-found-error'>
+            <img src="https://cdn5.vectorstock.com/i/1000x1000/73/49/404-error-page-not-found-miss-paper-with-white-vector-20577349.jpg" alt="" />
         </div>
     );
 };
